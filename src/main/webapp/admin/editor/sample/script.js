23bfe0b0636512cbdb988a9835df4f036f5a86c1
tmwmaldi96@gmail.com <tmwmaldi96@gmail.com>
2023-12-11 11:42:25 +0900

    CK EDITOR Intermediate Course

---
+++ src/main/webapp/admin/editor/sample/script.js
@@ -0,0 +1,20 @@
+ClassicEditor
+	.create( document.querySelector( '.editor' ), {
+		// Editor configuration.
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( handleSampleError );
+
+function handleSampleError( error ) {
+	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+	const message = [
+		'Oops, something went wrong!',
+		`Please, report the following error on ${ issueUrl } with the build id "ctz6t4g2amj4-9n1ju5wizfs7" and the error stack trace:`
+	].join( '\n' );
+
+	console.error( message );
+	console.error( error );
+}
