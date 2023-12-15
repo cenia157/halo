8d88d9a6e8c48af6e68465b3d0b7cec3f76cf155
allelouia <allelouia1574@gmail.com>
2023-12-15 17:02:58 +0900

    fix

---
+++ src/main/java/com/halp/admin/adminC2.java
@@ -0,0 +1,54 @@
+package com.halp.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/adminC")
+public class adminC2 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String menu = null;
+		if (request.getParameter("link") != null) {
+			if (request.getParameter("link").equals("1")) {
+				menu = "/admin/homepageSetting/logo/logoContent";
+			} else if (request.getParameter("link").equals("2")) {
+				menu = "/admin/homepageSetting/mainpage/mainpageContent";
+			} else if (request.getParameter("link").equals("3")) {
+				menu = "/admin/homepageSetting/banner/bannerContent";
+			} else if (request.getParameter("link").equals("4")) {
+				menu = "/admin/homepageSetting/information/informationContent";
+			} else if (request.getParameter("link").equals("5")) {
+				menu = "/admin/boardmanagement/notice/noticeContent";
+			} else if (request.getParameter("link").equals("6")) {
+				menu = "/admin/boardmanagement/ask/askContent";
+			} else if (request.getParameter("link").equals("7")) {
+				menu = "/admin/boardmanagement/frequenthyask/frequenthyaskContent";
+			} else if (request.getParameter("link").equals("8")) {
+				menu = "/admin/calender/company/companyContent";
+			} else if (request.getParameter("link").equals("9")) {
+				menu = "qa/question/question";
+			} else if (request.getParameter("link").equals("10")) {
+				menu = "qa/question/questionDetail";
+			} 
+			request.setAttribute("menu", menu + ".jsp");
+			request.getRequestDispatcher("admin/index.jsp").forward(request, response);
+			
+		} else {
+			request.setAttribute("menu", "/admin/dashboard/dashboard.jsp");
+			request.getRequestDispatcher("admin/index.jsp").forward(request, response);
+		}
+
+
+		String link = request.getParameter("link");
+		System.out.println(link);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
