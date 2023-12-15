28de2653e6b4a92e3e7e760291f673222b97cf57
allelouia <allelouia1574@gmail.com>
2023-12-15 16:44:42 +0900

    admin connection with jw

--- src/main/java/com/halp/admin/adminC.java
+++ src/main/java/com/halp/admin/adminC.java
@@ -11,8 +11,40 @@ import javax.servlet.http.HttpServletResponse;
 public class adminC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		request.getRequestDispatcher("admin/index.jsp").forward(request, response);
+		String menu = null;
+		if (request.getParameter("link") != null) {
+			if (request.getParameter("link").equals("1")) {
+				menu = "/admin/homepageSetting/logo/logoContent";
+			} else if (request.getParameter("link").equals("2")) {
+				menu = "/admin/homepageSetting/mainpage/mainpageContent";
+			} else if (request.getParameter("link").equals("3")) {
+				menu = "/admin/homepageSetting/banner/bannerContent";
+			} else if (request.getParameter("link").equals("4")) {
+				menu = "/admin/homepageSetting/information/informationContent";
+			} else if (request.getParameter("link").equals("5")) {
+				menu = "/admin/boardmanagement/notice/noticeContent";
+			} else if (request.getParameter("link").equals("6")) {
+				menu = "/admin/boardmanagement/ask/askContent";
+			} else if (request.getParameter("link").equals("7")) {
+				menu = "/admin/boardmanagement/frequenthyask/frequenthyaskContent";
+			} else if (request.getParameter("link").equals("8")) {
+				menu = "/admin/calender/company/companyContent";
+			} else if (request.getParameter("link").equals("9")) {
+				menu = "qa/question/question";
+			} else if (request.getParameter("link").equals("10")) {
+				menu = "qa/question/questionDetail";
+			} 
+			request.setAttribute("menu", menu + ".jsp");
+			request.getRequestDispatcher("admin/index.jsp").forward(request, response);
+			
+		} else {
+			request.setAttribute("menu", "/admin/dashboard/dashboard.jsp");
+			request.getRequestDispatcher("admin/index.jsp").forward(request, response);
+		}
+
+
+		String link = request.getParameter("link");
+		System.out.println(link);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
