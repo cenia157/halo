ce81d20e1ac292a8b743c802afefb613a941c927
tmwmaldi96@gmail.com <tmwmaldi96@gmail.com>
2023-12-18 18:31:00 +0900

    12-18 localhost 현재시간 가져와서 저장 확인, 다음은 ip주소를 가져와서 저장해야한다 또한 중요하게 바뀐점, 고쳐야 할 점이 있다
    중요하게 바뀐 점 :
    AdminC 이 부분에서 37줄
    else if (request.getParameter("link").equals("11")) {
         menu = "/admin/customerCare/stats/stats";
       }
    이 부분이 추가도었다는 점
    
    c 빠진 점 css 확인하고 고치기

---
+++ src/main/java/com/halo/test/DBManager.java
@@ -0,0 +1,35 @@
+package com.halo.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBManager {
+
+	public static Connection connect() throws SQLException {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		return DriverManager.getConnection(url,"c##kjw9600","kjw9600");
+	}
+
+	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+}
