febb4dde5be61825a93b6b81c0f3a0ec8d15a38a
tmwmaldi96@gmail.com <tmwmaldi96@gmail.com>
2023-12-19 16:11:32 +0900

    11111

---
+++ src/main/java/com/halo/test/DBManagerhalo.java
@@ -0,0 +1,36 @@
+package com.halo.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBManagerhalo {
+
+	public static Connection connect() throws SQLException, ClassNotFoundException {
+		Class.forName("oracle.jdbc.OracleDriver");
+		String url = "jdbc:oracle:thin:@anxucl0a8nyvimp0_high?TNS_ADMIN=C:\\Wallet_ANXUCL0A8NYVIMP0";
+		return DriverManager.getConnection(url, "ADMIN", "Soldesk802!!");
+	}
+
+	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+}
