7d5e062af33eee45f6f60f03d5055ce10f96011a
ody <rurii@knou.ac.kr>
2023-12-27 19:54:25 +0900

    23-12-27 commit

--- src/main/java/com/halo/test/DBManagerhalo_ody.java
+++ src/main/java/com/halo/test/DBManagerhalo_ody.java
@@ -15,8 +15,8 @@ public class DBManagerhalo_ody {
 	static {
 		dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
-        dataSource.setUrl("jdbc:oracle:thin:@anxucl0a8nyvimp0_high?TNS_ADMIN=C:\\\\Wallet_ANXUCL0A8NYVIMP0");
 		dataSource.setUrl("jdbc:oracle:thin:@anxucl0a8nyvimp0_high?TNS_ADMIN=/Users/admin/mg/Wallet_ANXUCL0A8NYVIMP0");
+	    dataSource.setUrl("jdbc:oracle:thin:@anxucl0a8nyvimp0_high?TNS_ADMIN=C:/Wallet_ANXUCL0A8NYVIMP0");
 		dataSource.setUsername("ADMIN");
 		dataSource.setPassword("Soldesk802!!");
 //        dataSource.setInitialSize(5); // 초기 연결 수
