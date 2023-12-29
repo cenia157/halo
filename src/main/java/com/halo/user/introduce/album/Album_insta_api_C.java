787374def9e8c2e8cd87f1ef33c98503705c6747
ody <rurii@knou.ac.kr>
2023-12-29 17:37:05 +0900

    토큰 재발급 ㅠ

--- src/main/java/com/halo/user/introduce/album/Album_insta_api_C.java
+++ src/main/java/com/halo/user/introduce/album/Album_insta_api_C.java
@@ -7,7 +7,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletContext;
+
+import com.halo.main.MainpageDAO;
 
 @WebServlet("/Album_insta_api_C")
 public class Album_insta_api_C extends HttpServlet {
@@ -17,8 +18,9 @@ public class Album_insta_api_C extends HttpServlet {
     	// 토큰 보안을 위해서 프로퍼티 파일을 만들어서 사용
         String configPath = "/user/introduce/album/token_config.properties";
         String apiToken = TokenConfigReader.loadApiToken(configPath, getServletContext());
-    
+        
         request.setAttribute("apiToken", apiToken);
+        MainpageDAO.getMdao().getAllHompage_common(request);
         request.setAttribute("menu", "user/menu-index.jsp");
         request.setAttribute("subMenu", "/user/introduce/album/album_contentPage.jsp");
         request.getRequestDispatcher("index.jsp").forward(request, response);
