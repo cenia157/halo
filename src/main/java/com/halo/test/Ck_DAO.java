e100d27c902be9dcf0def42831adbb62b9cf1c07
ody <rurii@knou.ac.kr>
2024-01-02 09:38:07 +0900

    커밋

--- src/main/java/com/halo/test/Ck_DAO.java
+++ src/main/java/com/halo/test/Ck_DAO.java
@@ -14,31 +14,38 @@ public class Ck_DAO {
 			String title = request.getParameter("title");
 			String select = request.getParameter("select");
 			String txt = request.getParameter("txt");
-			System.out.println("txt :::" + txt);
-			String saveFnameValues[] = request.getParameterValues("saveFname");
-			System.out.println("saveFnameValues :::" + saveFnameValues[0]);
-			System.out.println("saveFnameValues :::" + saveFnameValues[1]);
+			System.out.println("txt 구간(1) :::" + txt);
+//			String saveFnameValues[] = request.getParameterValues("saveFname");
+//			System.out.println("saveFnameValues :::" + saveFnameValues[0]);
+//			System.out.println("saveFnameValues :::" + saveFnameValues[1]);
+			String[] saveFnameValues = request.getParameterValues("saveFname");
 
-//			if (saveFnameValues != null) {
-//			    String[] saveArray = saveFnameValues.split("&");
-//
-//			    for (int i = 0; i < saveArray.length; i++) {
-//			        // 각각의 img 태그에 대해 대체 수행
-//			        txt = txt.replace("img", "img src=\'" + saveArray[i] + "'");
-//
-//			        System.out.println("saveFName: " + saveArray[i]);
-//			    }
-//			}
+			if (saveFnameValues != null) {
+			    int startPos = 0;  // 현재 위치를 추적하기 위한 변수
+			    for (int i = 0; i < saveFnameValues.length; i++) {
+			        // 다음 <img 태그의 시작 위치를 찾습니다.
+			        int imgPos = txt.indexOf("<img", startPos);
+			        if (imgPos == -1) break;  // 더 이상 <img 태그가 없으면 반복을 중단합니다.
 
-			
+			        // <img 태그를 대체할 새로운 문자열을 생성합니다.
+			        String toReplace = "<img src=\'" + saveFnameValues[i] + "'";
 
+			        // 기존의 <img 태그를 새로운 문자열로 대체합니다.
+			        txt = txt.substring(0, imgPos) + toReplace + txt.substring(txt.indexOf(">", imgPos) + 1);
+			        
+			        // 현재 위치를 업데이트합니다.
+			        startPos = imgPos + toReplace.length();
 
+			        System.out.println("txt 구간(2) :::" + txt);
+			        System.out.println("saveFName: " + saveFnameValues[i]);
+			    }
+			}
 
 			System.out.println(title);
 			System.out.println(select);
 //			System.out.println(saveFName);
 //			txt = txt.replace("img", "img src=\'" + saveFName + "\'");
-			System.out.println(txt);
+			
 
 			Connection con = null;
 			PreparedStatement pstmt = null;
--- src/main/java/com/halo/user/introduce/album/Album_insta_api_C.java
+++ src/main/java/com/halo/user/introduce/album/Album_insta_api_C.java
@@ -1,6 +1,10 @@
 package com.halo.user.introduce.album;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,9 +28,56 @@ public class Album_insta_api_C extends HttpServlet {
         request.setAttribute("menu", "user/menu-index.jsp");
         request.setAttribute("subMenu", "/user/introduce/album/album_contentPage.jsp");
         request.getRequestDispatcher("index.jsp").forward(request, response);
+        
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    	requestInstagram(request, response);
+    }
+    
+    /**
+     * 인스타 api 요청
+     * @param request
+     * @param response
+     */
+    private void requestInstagram(HttpServletRequest request, HttpServletResponse response) {
+    	try {
+            // 요청을 보낼 URL 생성
+    		String configPath = "/user/introduce/album/token_config.properties";
+    		String apiToken = TokenConfigReader.loadApiToken(configPath, getServletContext());
+            String urlString = "https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url&access_token="+apiToken;
+            URL url = new URL(urlString);
+
+            // HttpURLConnection 객체 생성
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // GET 메서드를 사용하도록 설정
+            connection.setRequestMethod("GET");
+
+            // 응답 코드 확인 (200은 성공을 의미)
+            int responseCode = connection.getResponseCode();
+            System.out.println("응답 코드: " + responseCode);
+
+            // 응답 내용 읽기
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder responseBuilder = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+            	responseBuilder.append(line);
+            }
+            reader.close();
+
+            // 응답 내용 출력
+            System.out.println("응답 내용: " + responseBuilder.toString());
+            
+            response.getWriter().write(responseBuilder.toString());
+
+            // 연결 종료
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
