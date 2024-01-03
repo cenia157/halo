5897f6427ebfc750cb5b126637fd0791099ac6b3
ody <rurii@knou.ac.kr>
2024-01-03 20:24:32 +0900

    커밋

--- src/main/webapp/admin/boardmanagement/notice_test/0.js/testMZ.js
+++ src/main/webapp/admin/boardmanagement/notice_test/0.js/testMZ.js
@@ -1,10 +1,8 @@
 class MyUploadAdapter {
   constructor(loader) {
-    // The file loader instance to use during the upload.
     this.loader = loader;
   }
 
-  // Starts the upload process.
   upload() {
     return this.loader.file.then(
       (file) =>
@@ -16,26 +14,18 @@ class MyUploadAdapter {
     );
   }
 
-  // Aborts the upload process.
   abort() {
     if (this.xhr) {
       this.xhr.abort();
     }
   }
 
-  // Initializes the XMLHttpRequest object using the URL passed to the constructor.
   _initRequest() {
     const xhr = (this.xhr = new XMLHttpRequest());
-
-    // Note that your request may look different. It is up to you and your editor
-    // integration to choose the right communication channel. This example uses
-    // a POST request with JSON as a data structure but your configuration
-    // could be different.
     xhr.open("POST", "/halo/CKEditorAjax", true);
     xhr.responseType = "json";
   }
 
-  // Initializes XMLHttpRequest listeners.
   _initListeners(resolve, reject, file) {
     const xhr = this.xhr;
     const loader = this.loader;
@@ -46,35 +36,23 @@ class MyUploadAdapter {
     xhr.addEventListener("load", () => {
       const response = xhr.response;
 
-      // This example assumes the XHR server's "response" object will come with
-      // an "error" which has its own "message" that can be passed to reject()
-      // in the upload promise.
-      //
-      // Your integration may handle upload errors in a different way so make sure
-      // it is done properly. The reject() function must be called when the upload fails.
       if (!response || response.error) {
+        console.log(
+          `File ${
+            file.name
+          } failed to upload at ${new Date().toLocaleTimeString()}`
+        );
         return reject(
           response && response.error ? response.error.message : genericErrorText
         );
       }
 
-      // If the upload is successful, resolve the upload promise with an object containing
-      // at least the "default" URL, pointing to the image on the server.
-      // This URL will be used to display the image in the content. Learn more in the
-      // UploadAdapter#upload documentation.
-      resolve({
-        default: response.url,
-      });
-      console.log(response);
-      console.log("~~~~~~~~~ success call!!!");
-
-      /* 
-			이건 백업용	
-			document.querySelector('figure').firstChild.src = response.fName;
-			document.querySelector('#img-url').value = response.fName;
-			*/
-
-      /*			var newInput = document.querySelector('#img-temporary');*/
+      resolve({ default: response.url });
+      console.log(
+        `File ${
+          file.name
+        } uploaded successfully at ${new Date().toLocaleTimeString()}`
+      );
 
       var newInput = document.createElement("input");
       var targetDiv = document.getElementById("img-temporary");
@@ -85,28 +63,10 @@ class MyUploadAdapter {
       nextInput.value = response.fName;
 
       document.querySelector("figure").lastChild.src = response.fName;
-	  document.querySelector('.ck-content').click();
-      /*			document.querySelector('figure').lastElementChild.src = response.fName;
-						document.querySelector('#img-url' + 1).value = response.fName;
-			
-			
-			
-			
-						targetDiv.appendChild(newInput);
-			
-			
-						var last = targetDiv.lastElementChild;
-			
-						console.log(targetDiv);
-						console.log(newInput);
-						console.log(last);
-			
-			*/
+ 	  document.querySelector('.ck-content').click();
+      console.log(`File ${file.name} displayed in the content.`);
     });
 
-    // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
-    // properties which are used e.g. to display the upload progress bar in the editor
-    // user interface.
     if (xhr.upload) {
       xhr.upload.addEventListener("progress", (evt) => {
         if (evt.lengthComputable) {
@@ -117,40 +77,47 @@ class MyUploadAdapter {
     }
   }
 
-  // Prepares the data and sends the request.
   _sendRequest(file) {
-    // Prepare the form data.
     const data = new FormData();
-
     data.append("upload", file);
-
-    // Important note: This is the right place to implement security mechanisms
-    // like authentication and CSRF protection. For instance, you can use
-    // XMLHttpRequest.setRequestHeader() to set the request headers containing
-    // the CSRF token generated earlier by your application.
-
-    // Send the request.
     this.xhr.send(data);
   }
 }
 
 function MyCustomUploadAdapterPlugin(editor) {
   editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
-    // Configure the URL to the upload script in your back-end here!
     return new MyUploadAdapter(loader);
   };
 }
 
+async function uploadFilesSequentially(editor, files) {
+  for (const file of files) {
+    console.log(
+      `Starting upload for ${file.name} at ${new Date().toLocaleTimeString()}`
+    );
+    const loader = editor.plugins.get("FileRepository").createLoader(file);
+    if (loader) {
+      await loader.file.then((file) => {
+        return new Promise((resolve, reject) => {
+          const myUploadAdapter = new MyUploadAdapter(loader);
+          myUploadAdapter.upload().then(resolve).catch(reject);
+        });
+      });
+    }
+  }
+  console.log("All files have been uploaded sequentially");
+}
+
 ClassicEditor.create(document.querySelector("#classicNR"), {
   extraPlugins: [MyCustomUploadAdapterPlugin],
-
-  // More configuration options.
-  // ...
 })
   .then((editor) => {
     window.editor = editor;
+    const files = document.querySelector('input[type="file"]').files;
+    uploadFilesSequentially(editor, files).then(() => {
+      console.log("All files have been processed");
+    });
   })
   .catch((error) => {
-    console.log(error);
+    console.error("Error occurred:", error);
   });
-// 센세 항상 감사합니다ㅠㅠ
